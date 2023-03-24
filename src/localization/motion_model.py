6bde1d2783aa39c4d0e25ba2cdd8ea1c48600ea7
hgheiberger <harryh@mit.edu>
2023-03-24 20:57:12 +0000

    Add noise to odometry measurement

--- src/localization/motion_model.py
+++ src/localization/motion_model.py
@@ -5,7 +5,9 @@ class MotionModel:
 
     def __init__(self):
         self.deterministic = rospy.get_param("~deterministic")
+        self.num_particles = rospy.get_param("~num_particles")
         self.noise_st_dev = 0.3
+
 
     def evaluate(self, particles, odometry):
         """
@@ -25,14 +27,22 @@ class MotionModel:
             particles: An updated matrix of the
                 same size
         """
-        if not self.deterministic:
-            # add Gaussian noise
-            odometry += np.random.normal(0, self.noise_st_dev, 3)
-        particles[:,0] = (np.cos(particles[:,2]) * odometry[0]
-                              - np.sin(particles[:,2]) * odometry[1]
-                              + particles[:,0])
-        particles[:,1] = (np.sin(particles[:,2]) * odometry[0]
-                              + np.cos(particles[:,2]) * odometry[1]
-                              + particles[:,1])
-        particles[:,2] += odometry[2]
+        if self.deterministic:
+            particles[:,0] = (np.cos(particles[:,2]) * odometry[0]
+                                - np.sin(particles[:,2]) * odometry[1]
+                                + particles[:,0])
+            particles[:,1] = (np.sin(particles[:,2]) * odometry[0]
+                                + np.cos(particles[:,2]) * odometry[1]
+                                + particles[:,1])
+            particles[:,2] += odometry[2]
+
+        else:
+            # Create guassian noise
+            odom_matrix = np.tile(odometry, (self.num_particles,1))
+            odom_matrix += np.random.normal(0.0,self.noise_st_dev, (self.num_particles,3))
+            
+            # Update particle positions
+            particles[:,0] = np.multiply(np.cos(particles[:,2]),odom_matrix[:,0]) - np.multiply(np.sin(particles[:,2]), odom_matrix[:,1]) + particles[:,0]
+            particles[:,1] = np.multiply(np.sin(particles[:,2]), odom_matrix[:,0]) + np.multiply(np.cos(particles[:,2]), odom_matrix[:,1]) + particles[:,1]
+            particles[:,2] += odom_matrix[:,2]
         return particles
