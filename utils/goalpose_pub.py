046a40ae4e7e7ea7e992c5791c5ff0c3a983d87a
hgheiberger <hgheiberger@gmail.com>
2023-04-18 00:50:57 -0400

    Fix mismatchced type error in goalpose

--- utils/goalpose_pub.py
+++ utils/goalpose_pub.py
@@ -2,7 +2,7 @@
 
 import sys
 import rospy
-from geometry_msgs.msg import PoseWithCovarianceStamped
+from geometry_msgs.msg import PoseStamped
 from tf.transformations import quaternion_from_euler
 
 if __name__ == "__main__":
@@ -11,15 +11,15 @@ if __name__ == "__main__":
     y = float(sys.argv[2])
     theta = float(sys.argv[3])
 
-    pub = rospy.Publisher("/move_base_simple/goal", PoseWithCovarianceStamped, queue_size=1, latch=True)
-    msg = PoseWithCovarianceStamped()
-    msg.pose.pose.position.x = x
-    msg.pose.pose.position.y = y
+    pub = rospy.Publisher("/move_base_simple/goal", PoseStamped, queue_size=1, latch=True)
+    msg = PoseStamped()
+    msg.pose.position.x = x
+    msg.pose.position.y = y
     o = quaternion_from_euler(0,0,theta)
-    msg.pose.pose.orientation.x = o[0]
-    msg.pose.pose.orientation.y = o[1]
-    msg.pose.pose.orientation.z = o[2]
-    msg.pose.pose.orientation.w = o[3]
+    msg.pose.orientation.x = o[0]
+    msg.pose.orientation.y = o[1]
+    msg.pose.orientation.z = o[2]
+    msg.pose.orientation.w = o[3]
 
     pub.publish(msg)
     rospy.spin()
