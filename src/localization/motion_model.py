b5aebf85603ae67aec138386f9492152f2e30dc1
hgheiberger <hgheiberger@gmail.com>
2023-04-18 02:46:41 -0400

    Revert localization to original configuration

--- src/localization/motion_model.py
+++ src/localization/motion_model.py
@@ -13,13 +13,16 @@ class MotionModel:
         """
         Update the particles to reflect probable
         future states given the odometry data.
+
         args:
             particles: An Nx3 matrix of the form:
             
                 [x0 y0 theta0]
                 [x1 y0 theta1]
                 [    ...     ]
+
             odometry: A 3-vector [dx dy dtheta]
+
         returns:
             particles: An updated matrix of the
                 same size
